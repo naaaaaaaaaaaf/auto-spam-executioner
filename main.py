9b4516e6528e43e527cbf6d8d34034ddf1ecc362
Takato Ueno <naf@auriga.dev>
2024-02-20 14:05:14 +0900

    複数サーバーのストリーミングを監視するように変更

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from mastodon import Mastodon, StreamListener
-from dotenv import load_dotenv
 import os
-import pprint
-import time
-import urllib.error
-import urllib.request
-import datetime
-
-# .envの読み込み
-load_dotenv()
-
-# Mastodonのクライアント生成
-client = Mastodon(api_base_url = os.environ['API_BASE_URL'], access_token = os.environ['ACCESS_TOKEN'])
+import threading
+import json
 
 # 連合タイムラインをListenするためのクラスを定義
 class PublicStreamListener(StreamListener):
@@ -29,12 +19,12 @@ class PublicStreamListener(StreamListener):
 
     def on_update(self, status):
         try:
-            # メンション数が規定数より多く、かつ外部からの投稿の場合
-            if len(status.mentions) > int(os.environ['MEMTION_COUNT']) and status.account.acct.find('@') > 0:
+            if len(status.mentions) > 3 and status.account.acct.find('@') > 0 and status.account.followers_count < 3:
                 statusID = status.id
                 account = status.account
 
                 # スパムの通報(リモートサーバにも転送)を投げ、アカウントを停止させる
+                print("spam!")
                 report = self.client.report(account.id, status_ids=[statusID], forward=True, category='spam')
                 self.client.admin_account_moderate(account.id, 'suspend', report_id=report.id)
 
@@ -42,8 +32,15 @@ class PublicStreamListener(StreamListener):
             print(e)
             pass
 
-# 連合タイムラインのListenerを生成            
-public_stream_listener = PublicStreamListener(client)
+def start_listener(instance):
+    client = Mastodon(api_base_url=instance['api_base_url'], access_token=instance['access_token'])
+    listener = PublicStreamListener(client)
+    client.stream_public(listener, remote=True)
 
-# 連合タイムラインをListen
-client.stream_public(public_stream_listener, remote=True)
+# 設定ファイルからインスタンス情報を読み込む
+with open('config.json', 'r') as file:
+    instances = json.load(file)
+
+# 各インスタンスに対してリスナーをスレッドで起動
+for instance in instances:
+    threading.Thread(target=start_listener, args=(instance,)).start()
